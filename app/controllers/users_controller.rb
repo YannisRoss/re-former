87c37b4bae83626617a5c335a8cd814a97e95df0
Grimhamr <rossiadisj@gmail.com>
2021-08-15 18:43:10 +0300

    add edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,28 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit
+        @user = User.find(params[:id])
+
+        #if @user.save
+        #    redirect_to edit_user_path	
+        #else
+        #    render :new
+        #end
+    end
+
+    def update
+        #@user = User.find(params[:id])
+        if request.post?
+            redirect_to new_user_path if(@user.save)
+            
+        else
+          render :new
+        end
+      
+      
+    end
+
     def create
         #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
         @user = User.new(user_params)
